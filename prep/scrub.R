c413b0238567e9d76f866846d2add6c0079f5cc3
Joshh <josh.hagedorn@gmail.com>
2016-09-06 09:33:35 -0400

    Modify to include PECFAS

--- prep/scrub.R
+++ prep/scrub.R
@@ -8,28 +8,28 @@ library(car)
 # Define dataset without PHI to use with Shiny Apps 
 
   # Make an ID key
-    id <- unique(sub_cafas$id)
-    cafas_key <- data.frame(id)
-    cafas_key$fake_id <- sample(x = 100000001:999999999, 
-                              size = length(cafas_key$id), 
+    id <- unique(sub_fas$id)
+    fas_key <- data.frame(id)
+    fas_key$fake_id <- sample(x = 100000001:999999999, 
+                              size = length(fas_key$id), 
                               replace = FALSE)
-    cafas_key$id <- as.character(cafas_key$id)
-    cafas_key$fake_id <- as.character(cafas_key$fake_id)
+    fas_key$id <- as.character(fas_key$id)
+    fas_key$fake_id <- as.character(fas_key$fake_id)
     rm(id)
 
   # Make an episode key
-    unique_episode_id <- unique(sub_cafas$unique_episode_id)
-    cafas_episode_key <- data.frame(unique_episode_id)
-    cafas_episode_key$fake_episode_id <- sample(x = 100000001:999999999, 
-                                        size = length(cafas_episode_key$unique_episode_id), 
-                                        replace = FALSE)
-    cafas_episode_key$unique_episode_id <- as.character(cafas_episode_key$unique_episode_id)
-    cafas_episode_key$fake_episode_id <- as.character(cafas_episode_key$fake_episode_id)
+    unique_episode_id <- unique(sub_fas$unique_episode_id)
+    fas_episode_key <- data.frame(unique_episode_id)
+    fas_episode_key$fake_episode_id <- sample(x = 100000001:999999999, 
+                                              size = length(fas_episode_key$unique_episode_id), 
+                                              replace = FALSE)
+    fas_episode_key$unique_episode_id <- as.character(fas_episode_key$unique_episode_id)
+    fas_episode_key$fake_episode_id <- as.character(fas_episode_key$fake_episode_id)
     rm(unique_episode_id)  
     
   # Make PHI-free dataset
-    scrub_cafas <-
-      sub_cafas %>%
+    scrub_fas <-
+      sub_fas %>%
       mutate(id = as.character(id)) %>%
       left_join(cafas_key, by = "id") %>%
       left_join(cafas_episode_key, by = "unique_episode_id") %>%
@@ -37,4 +37,4 @@ library(car)
       mutate(fake_id = as.factor(fake_id),
              fake_episode_id = as.factor(fake_episode_id))
     
-write.csv(scrub_cafas,"data/scrub_cafas.csv", row.names = F)
+write.csv(scrub_fas,"data/scrub_fas.csv", row.names = F)
